08b31de6ee01a722e6f5715b14ecd96337dbe057
doob187 <60312740+doob187@users.noreply.github.com>
2020-09-14 22:26:39 +0200

    Update deploy.sh

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -26,8 +26,8 @@ deploypgblitz() {
 }
 updatesystem() {
   # update system to new packages
-  sudo pip install --ignore-installed --upgrade ansible
-  ansible-playbook /opt/pgclone/ymls/update.yml
+  sudo pip install --ignore-installed --upgrade ansible 2>&1 >>/dev/null
+  ansible-playbook /opt/pgclone/ymls/update.yml 2>&1 >>/dev/null
 }
 deploypgmove() {
   # RCLONE BUILD
@@ -62,7 +62,7 @@ apt-get install ethtool vnstat vnstati -yqq 2>&1 >>/dev/null
 export DEBIAN_FRONTEND=noninteractive
 network=$(ifconfig | grep -E 'eno1|enp|ens5' | awk '{print $1}' | sed -e 's/://g')
 sed -i 's/eth0/'$network'/g' /etc/vnstat.conf
-sed -i '/UseLogging/s/2/0/' /etc/vnstat.conf \
+sed -i '/UseLogging/s/2/0/' /etc/vnstat.conf
 sed -i '/RateUnit/s/1/0/' /etc/vnstat.conf
 sed -i '/UnitMode/s/0/1/' /etc/vnstat.conf
 sed -i 's/Locale "-"/Locale "LC_ALL=en_US.UTF-8"/g' /etc/vnstat.conf
