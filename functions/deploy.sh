1bc31ab8606540e1986b8ceb1cabbaddc9de6df8
halomore <48292010+smashingtags@users.noreply.github.com>
2020-09-29 20:38:24 -0400

    Update deploy.sh
    changed 90 second timer to 5 seconds

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -208,7 +208,7 @@ EOF
 
 ########################################################################################
 timer() {
-seconds=90; date1=$((`date +%s` + $seconds)); 
+seconds=5; date1=$((`date +%s` + $seconds)); 
 while [ "$date1" -ge `date +%s` ]; do 
   echo -ne "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r"; 
 done
