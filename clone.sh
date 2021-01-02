6d9815f91fb87c891629373490e4dd8f87bf7f31
Aelfa <60222501+aelfa@users.noreply.github.com>
2021-01-02 22:01:23 +0500

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -14,7 +14,7 @@ function clone() {
     sudo rm -rf /opt/pgclone
     curl -fsSL https://raw.githubusercontent.com/MatchbookLab/local-persist/master/scripts/instal l.sh | sudo bash
     sudo docker volume create -d local-persist -o mountpoint=/mnt --name=unionfs 
-    sudo git clone --quiet https://github.com/doob187/pgclone.git /opt/pgclone
+    sudo git clone --quiet https://github.com/aelfa/pgclone.git /opt/pgclone
     sudo chown -cR 1000:1000 /opt/pgclone/ 1>/dev/null 2>&1
     sudo chmod -cR 755 /opt/pgclone >> /dev/null 1>/dev/null 2>&1
     sudo bash /opt/pgclone/pgclone.sh
